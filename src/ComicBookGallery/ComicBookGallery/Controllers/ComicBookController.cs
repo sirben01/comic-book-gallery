625e64fc2a8ebec1938473b9cd857fd8e10814ca
Benjamin Pack <benlevipack@gmail.com>
2017-05-28 19:24:05 -0400

    Added View and Changed our ComicBookController to return to the view.

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
@@ -11,12 +11,7 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-
-            }
-            return Content("Hello from the ComicBookController action method called detail!");
+            return View();
         }
 
     }
